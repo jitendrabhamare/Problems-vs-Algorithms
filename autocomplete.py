489c9fd1bbd0a7bc11c2d45e01f7f47d5d3be9ba
Jitendra Bhamare <jitendra0905@gmail.com>
2019-07-25 00:32:26 -0700

    Updated Autocomplete code

--- autocomplete.py
+++ autocomplete.py
@@ -9,7 +9,33 @@ class TrieNode:
     def insert(self, char):
         ## Add a child node in this Trie
         self.children[char] = TrieNode()
+
+    def suffixes(self, suffix = '', suffix_list = None):
+        ## Recursive function that collects the suffix for 
+        ## all complete words below this point
+        if suffix_list == None:
+            suffix_list = []
+
+        ## When node is empty, it's leaf node, append it and return
+        if self.children.keys() == []:
+            suffix_list.append(suffix)
+            return
+
+        ## if append it to suffix_list if we find a word.
+        if self.word_finished == True:
+            suffix_list.append(suffix)
         
+        ## Recursively call 'suffixes' (DFS) and form string from children keys 
+        for key in self.children.keys():
+            self.children[key].suffixes(suffix+key, suffix_list)
+        
+        ## Remove an empty string from list
+        suffix_list = ' '.join(suffix_list).split()
+        return suffix_list
+        #print("my suff: {}".format(self.suffix_list))
+
+
+
 ## The Trie itself containing the root node and insert/find functions
 class Trie:
     def __init__(self):
@@ -35,27 +61,34 @@ class Trie:
                 return False
             current_node = current_node.children[char]
         
-        return current_node.word_finished
-        #return current_node
+        return current_node
 
 
-word_list = ['apple', 'bear', 'goo', 'good', 'goodbye', 'goods', 'goodwill', 'gooses'  ,'zebra']
-word_trie = Trie()
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym", 
+    "fun", "function", "factory", 
+    "trie", "trigger", "trigonometry", "tripod"
+]
+for word in wordList:
+    MyTrie.insert(word) 
 
-## Add words
-for word in word_list:
-    word_trie.insert(word)
-
-print(word_trie)
-
-# Test words
-test_words = ['bear', 'goo', 'good', 'goos']
-for word in test_words:
-    #print(word_trie.find(word).children)
-    if word_trie.find(word):
-        print('"{}" is a word.'.format(word))
+def f(prefix):
+    if prefix != '':
+        prefixNode = MyTrie.find(prefix)
+        if prefixNode:
+            print('\n'.join(prefixNode.suffixes()))
+        else:
+            print(prefix + " not found")
     else:
-        print('"{}" is not a word.'.format(word))
+        print('')
 
+## Call function
+prefix_list = ['f', 'a', 'an', 'tr', 't', '', 'q']
 
-    
+for prefix in prefix_list:
+    print("input_text: {}".format(prefix))
+    print("output: ")
+    f(prefix)
+    print("\n")
+
